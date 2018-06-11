ee9ef7c30e10657d95611791fe0e5dbdcb3409f4
Bob Clarke <bobmclarke@gmail.com>
2018-06-11 22:20:42 +0100

    Trigger

--- pipeline/sonar.sh
+++ pipeline/sonar.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 echo "This is sonar.sh"
+
+npm i sonar-scanner
+
+echo 'sonar.host.url=http://sonarqube-sonarqube:9000' > sonar-scanner.properties
+echo 'sonar.projectKey=dandevops:nodejs-hello' >> sonar-project.properties
+echo 'sonar.projectName=NodeJS Hello World' >> sonar-project.properties
+echo 'sonar.projectVersion=1.0' >> sonar-project.properties
+echo 'sonar.sources=.' >> sonar-project.properties 
+./node_modules/sonar-scanner/bin/sonar-scanner -Dsonar.login=admin -Dsonar.password=admin
+
+
