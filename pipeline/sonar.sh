0a9c6f38789dd124d74d46382f3458e91ba9b5d7
Bob Clarke <bobmclarke@gmail.com>
2018-06-11 23:10:32 +0100

    Changing sonar sources

--- pipeline/sonar.sh
+++ pipeline/sonar.sh
@@ -3,12 +3,13 @@ echo "This is sonar.sh"
 
 npm i sonar-scanner && chown -R 10000:10000 node_modules
 
-echo 'sonar.host.url=http://sonarqube-sonarqube:9000' > sonar-scanner.properties
-echo 'sonar.projectKey=dandevops:nodejs-hello' >> sonar-project.properties
-echo 'sonar.projectName=NodeJS Hello World' >> sonar-project.properties
+echo 'sonar.host.url=http://democluster-team1-sonarqube-sonarqube.democluster-team1.svc.cluster.local:9000' > sonar-scanner.properties
+echo 'sonar.projectKey=dandevops:demo' >> sonar-project.properties
+echo 'sonar.projectName=demo' >> sonar-project.properties
 echo 'sonar.projectVersion=1.0' >> sonar-project.properties
-echo 'sonar.sources=.' >> sonar-project.properties 
-./node_modules/sonar-scanner/bin/sonar-scanner -X -Dsonar.login=admin -Dsonar.password=admin -Dsonar.host.url=http://democluster-team1-sonarqube-sonarqube.democluster-team1.svc.cluster.local:9000
+echo 'sonar.sources=examples' >> sonar-project.properties 
+
+./node_modules/sonar-scanner/bin/sonar-scanner -X -Dsonar.login=admin -Dsonar.password=admin 
 
 chown -R 10000:10000 .scannerwork
 
