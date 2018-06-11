0fcced29f35a8324508e2da6c6b69126b8dcf8a1
Bob Clarke <bobmclarke@gmail.com>
2018-06-11 23:14:33 +0100

    trigger

--- pipeline/sonar.sh
+++ pipeline/sonar.sh
@@ -3,13 +3,14 @@ echo "This is sonar.sh"
 
 npm i sonar-scanner && chown -R 10000:10000 node_modules
 
-echo 'sonar.host.url=http://democluster-team1-sonarqube-sonarqube.democluster-team1.svc.cluster.local:9000' > sonar-scanner.properties
-echo 'sonar.projectKey=dandevops:demo' >> sonar-project.properties
-echo 'sonar.projectName=demo' >> sonar-project.properties
-echo 'sonar.projectVersion=1.0' >> sonar-project.properties
-echo 'sonar.sources=examples' >> sonar-project.properties 
-
-./node_modules/sonar-scanner/bin/sonar-scanner -X -Dsonar.login=admin -Dsonar.password=admin 
+./node_modules/sonar-scanner/bin/sonar-scanner -X \
+	-Dsonar.login=admin \
+	-Dsonar.password=admin \
+	-Dsonar.host.url=http://democluster-team1-sonarqube-sonarqube.democluster-team1.svc.cluster.local:9000 \
+	-Dsonar.projectKey=dandevops:demo \
+	-Dsonar.projectName=demo \
+	-Dsonar.projectVersion=1.0 \
+	-Dsonar.sources=examples
 
 chown -R 10000:10000 .scannerwork
 
