087d4cac231229e50f5e1405e11bb0db4d037b45
Bob Clarke <bobmclarke@gmail.com>
2018-06-11 22:48:38 +0100

    Sonar debug

--- pipeline/sonar.sh
+++ pipeline/sonar.sh
@@ -8,6 +8,6 @@ echo 'sonar.projectKey=dandevops:nodejs-hello' >> sonar-project.properties
 echo 'sonar.projectName=NodeJS Hello World' >> sonar-project.properties
 echo 'sonar.projectVersion=1.0' >> sonar-project.properties
 echo 'sonar.sources=.' >> sonar-project.properties 
-./node_modules/sonar-scanner/bin/sonar-scanner -Dsonar.login=admin -Dsonar.password=admin -Dsonar.host.url=http://democluster-team1-sonarqube-sonarqube.democluster-team1.svc.cluster.local:9000
+./node_modules/sonar-scanner/bin/sonar-scanner -X -Dsonar.login=admin -Dsonar.password=admin -Dsonar.host.url=http://democluster-team1-sonarqube-sonarqube.democluster-team1.svc.cluster.local:9000
 
 
